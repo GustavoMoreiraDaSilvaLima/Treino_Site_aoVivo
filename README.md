c76718a9dffec716d5b86a9c9720f309f18a35e3
GustavoMoreiraDaSilvaLima <gustavomoreira3108@hotmail.com>
2023-11-12 00:28:30 -0300

    feats: arquivo final

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Treino_Site_aoVivo
 Projeto para upar um site com a requisição GET forms e listar cadastro em tabela
+
+# Site
+a
+
+# Formulario
+
+formulario com registro em uma pagina proxima
+
+# Jogo da Velha
+
+Um jogo da velha para ser jogado com 2 pessoas
+
+# OBS:
+na parte Registrar.html nas linhas especificas 51, 101 - 107 foram capadas para poder funcionar o resposta.end.
+a linha 52 não existia foi adicionada para o site funcionar
--- index.js
+++ index.js
@@ -5,6 +5,148 @@ const host = '0.0.0.0';
 
 var listaUsuarios = [];
 
+function funcionajogo(requisicao,resposta){
+    resposta.end(`<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jogo da Velha</title>
+    <link rel="stylesheet" href="style.css" />
+    <style>
+    *{font-family: calibri; font-size: 30px;}
+    #jogo{
+        margin: 0 auto;
+        display: grid;  
+        grid-template-columns: 120px 120px 120px;  
+        grid-template-rows: 120px 120px 120px;
+    }
+    
+    #jogo div {
+        background-color: black;
+        border: 3px solid blue;
+        font-size: 80px;
+        color: #ffffff;
+        border-radius: 10px 10px 10px 10px;
+        padding-top	: 10px;
+        text-align: center;
+    }
+    </style>
+</head>
+<body>
+    <section id="jogo">
+        <!-- linha -->
+        <div class="quadro" data-linha="1" data-coluna="1"></div>
+        <div class="quadro" data-linha="1" data-coluna="2"></div>
+        <div class="quadro" data-linha="1" data-coluna="3"></div>
+        <!-- linha -->
+        <div class="quadro" data-linha="2" data-coluna="1"></div>
+        <div class="quadro" data-linha="2" data-coluna="2"></div>
+        <div class="quadro" data-linha="2" data-coluna="3"></div>
+        <!-- linha -->
+        <div class="quadro" data-linha="3" data-coluna="1"></div>
+        <div class="quadro" data-linha="3" data-coluna="2"></div>
+        <div class="quadro" data-linha="3" data-coluna="3"></div>
+    </section>
+
+    <h3>Jogador: <span id="jogadorDoTurno"></span></h3>
+    <button id="reiniciar">Reiniciar</button><br>
+    <a href="/" role="button">Voltar ao menu</a>
+    <script>var jogador;
+    var pontuacao;
+    
+    function iniciarJogo() {
+        jogador = 'X';
+    
+        pontuacao = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+    
+    
+        document.getElementById('jogadorDoTurno').textContent = jogador;
+        var quadros = document.querySelectorAll('.quadro');
+    
+        for (let quadro of quadros) {
+            quadro.textContent = '';
+            quadro.addEventListener('click', marcarQuadro);
+        }
+    }
+    
+    function marcarQuadro() {
+        if (this.textContent == "") {
+            this.textContent = jogador;
+    
+            var linha = this.dataset.linha - 1;
+            var coluna = this.dataset.coluna - 1;
+            pontuacao[linha][coluna] = jogador;
+            conferirResultado();
+            trocaJogador();
+        }
+    }
+    
+    function trocaJogador() {
+        if (jogador == 'X') {
+            jogador = 'O';
+        }
+        else {
+            jogador = 'X';
+        }
+        document.getElementById('jogadorDoTurno').textContent = jogador;
+    }
+    function conferirResultado() {
+        var possuiGanhador = false;
+    
+        //linha
+        for (var linha = 0; linha < 3; linha++) {
+            if (pontuacao[linha][0] != '' && pontuacao[linha][1] != '' && pontuacao[linha][2] != '') {
+                if (pontuacao[linha][0] == pontuacao[linha][1] && pontuacao[linha][0] == pontuacao[linha][2]) {
+                    possuiGanhador = true;
+                }
+            }
+        }
+    
+        //coluna
+        for (var coluna = 0; coluna < 3; coluna++) {
+            if (pontuacao[0][coluna] != '' && pontuacao[1][coluna] != '' && pontuacao[2][coluna] != '') {
+                if (pontuacao[0][coluna] == pontuacao[1][coluna] && pontuacao[0][coluna] == pontuacao[2][coluna]) {
+                    possuiGanhador = true;
+                }
+            }
+        }
+        //diagonais
+        if (pontuacao[0][0] != '' && pontuacao[1][1] != '' && pontuacao[2][2] != '') {
+            if (pontuacao[0][0] == pontuacao[1][1] && pontuacao[0][0] == pontuacao[2][2]) {
+                possuiGanhador = true;
+            }
+        }
+        if (pontuacao[2][0] != '' && pontuacao[1][1] != '' && pontuacao[0][2] != '') {
+            if (pontuacao[2][0] == pontuacao[1][1] && pontuacao[2][0] == [0][2]) {
+                possuiGanhador = true;
+            }
+        }
+    
+        if (possuiGanhador == true) {
+            encerrarJogo();
+        }
+    }
+    function encerrarJogo() {
+        alert('Jogador ' + jogador + ' Ganhou!!!');
+    
+        var quadros = document.querySelectorAll('.quadro');
+        for (let quadro of quadros) {
+            
+            quadro.removeEventListener('click', marcarQuadro);
+        }
+    }
+    
+    iniciarJogo();
+    document.getElementById('reiniciar').addEventListener('click', iniciarJogo);</script>
+</body>
+</html>`);
+}
 
 function processaCadastroUsuario(requisicao, resposta) {
     const usuario = {
@@ -46,7 +188,7 @@ function processaCadastroUsuario(requisicao, resposta) {
                         </tbody>
                     </table>
                     <a class="btn btn-primary" href="/" role="button">Voltar ao menu</a>
-                    <a class="btn btn-primary" href="/cadastroUsuario.html" role="button">Continuar cadastrando</a>
+                    <a class="btn btn-primary" href="/Registrar.html" role="button">Continuar cadastrando</a>
                 </body>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
                     integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
@@ -66,6 +208,7 @@ const app = express();
 
 app.use(express.static('./forms'));
 
+
 app.get('/', (requisicao, resposta) => {
     resposta.end(`
     <!DOCTYPE html>
@@ -77,6 +220,7 @@ app.get('/', (requisicao, resposta) => {
         <h1>MENU</h1>
         <ul>
             <li><a href="Registrar.html">Cadastar Usuário</a></li>
+            <li><a href="jogo">Jogo da Velha</a></li>
         </ul>
     </body>
     </html>`
@@ -86,6 +230,9 @@ app.get('/', (requisicao, resposta) => {
 })
 
 app.get('/Registrar', processaCadastroUsuario);
+
+app.get('/jogo',funcionajogo);
+
 
 app.listen(porta, host, () => {
     console.log(`Servidor executando na url http://${host}:${porta}`);
